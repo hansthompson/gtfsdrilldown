76c640fd58ce6628ce43fb8ff152dd069e33cd69
Charles Hans Thompson <hans.thompson1@gmail.com>
2016-08-21 16:33:06 -0800

    okay. once again

--- R/todays_previous_departures.R
+++ R/todays_previous_departures.R
@@ -1,5 +1,5 @@
 todays_previous_departures <- function(drilldown_obj) {
-drillldown_obj$todays_trip_departures %>%
+drilldown_obj$todays_trip_departures %>%
   filter(departure_time < ymd_hms(Sys.time())) %>%
   select(trip_id, route_id, direction_id, departure_time)
 }
